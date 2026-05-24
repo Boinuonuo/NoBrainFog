de8dac3415920ed26501db7990fc41c26e873cb5
Boinuonuo <boinuonuo@outlook.com>
2026-05-24 07:24:42 -0400

    Register Email IMAP adapter

--- adapters/__init__.py
+++ adapters/__init__.py
@@ -1,10 +1,14 @@
 from .discord_bot import NoBrainFogBot
+from .email_imap import EmailIMAPAdapter
 from .wechat_work import WeChatWorkBot
 
+
 def create_adapter(adapter_type, config):
-    if adapter_type == 'discord':
+    if adapter_type == "discord":
         return NoBrainFogBot(config)
-    elif adapter_type == 'wechat_work':
+    elif adapter_type == "wechat_work":
         return WeChatWorkBot(config)
+    elif adapter_type == "email_imap":
+        return EmailIMAPAdapter(config)
     else:
         raise ValueError(f"Unknown adapter type: {adapter_type}")
